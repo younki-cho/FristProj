f7292d2cfea585fd08a4b500d2cdc7dab98604f8
younki.cho <gloss62@naver.com>
2021-12-21 09:48:11 +0900

    주석 후 변경

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String [] args){
-		//System.out.println("Git");
+		//System.out.println("Git");kkk
 		System.out.println("heo");
 		System.out.println("heo Git");
 		System.out.println("Hi Git");
